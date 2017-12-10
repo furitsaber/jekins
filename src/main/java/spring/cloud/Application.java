f7e67a05413962509a5fda7a0f26668359c46af4
shaoshuang <1471405258@qq.com>
2017-12-10 17:22:37 +0800

    123

--- src/main/java/spring/cloud/Application.java
+++ src/main/java/spring/cloud/Application.java
@@ -1,4 +1,4 @@
-package application;
+package spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Appliction {
+public class Application {
 
 	public static void main(String[] args){
-		SpringApplication.run(Appliction.class, args);
-	}
+		SpringApplication.run(Application.class, args);
+
+    }
 }
